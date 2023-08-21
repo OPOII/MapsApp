4b67fcbeff94b3beed291598dcbffb9dce2bac1a
OPOII <bryancamili198@hotmail.com>
2023-08-20 23:32:37 -0500

    Componente minimapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+  public map?:Map;
+  @Input()lngLat?:[number,number]
+
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement)throw "Map Div not found";
+    if(!this.lngLat)throw 'LngLat cant be null';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive:false
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(this.map);
+  }
 
 }
